54dce1e2731b502adf28c22b8c06e7b200f1b040
ViniBiss02 <viniciusbiss02@gmail.com>
2025-04-19 23:28:35 -0300

    nothing important

--- learn_tkinter.py
+++ learn_tkinter.py
@@ -1,25 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 
-def button_clicked():
-    print(string_var.get())
-    string_var.set("Button clicked!")
-
-#window
 window = tk.Tk()
 window.title("Learn Tkinter")
+window.geometry("600x400")
 
-#tkinter variables
-string_var = tk.StringVar()
 
-label = tk.Label(master = window, text="Label", textvariable=string_var)
-label.pack()
 
-entry = tk.Entry(master = window, textvariable=string_var)
-entry.pack()
-
-button = ttk.Button(master = window, text="Button", command=button_clicked)
-button.pack()
-
-#running
 window.mainloop()
