5945831e12a986ccfa10c6d74a56f4f9483b7ebc
ViniBiss02 <viniciusbiss02@gmail.com>
2025-05-05 20:42:56 -0300

    aprendendo sobre "command=lambda:"

--- learn_tkinter.py
+++ learn_tkinter.py
@@ -1,10 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 
+nome_usuario = "vinicius"
+
 window = tk.Tk()
 window.title("Learn Tkinter")
 window.geometry("600x400")
 
+def button_function(nome):
+    print(f"hello {nome}")
 
+button = ttk.Button(master = window, text="Click Me!", command=lambda: button_function(nome_usuario))
+button.pack()
 
 window.mainloop()
