d12784a2f6ae7847a0bdd5a990ca3744d70a82cf
ViniBiss02 <viniciusbiss02@gmail.com>
2025-05-05 21:30:01 -0300

    aprendendo sobre "radio button"

--- learn_tkinter.py
+++ learn_tkinter.py
@@ -7,10 +7,32 @@ window = tk.Tk()
 window.title("Learn Tkinter")
 window.geometry("600x400")
 
+check_var = tk.BooleanVar()
+check_radio_var = tk.BooleanVar()
+
 def button_function(nome):
-    print(f"hello {nome}")
+    if check_var.get() and check_radio_var.get() == True:
+        print(f"hello {nome} - check is checked")
+    else:
+        print(f"hello {nome} - check is not checked")
 
 button = ttk.Button(master = window, text="Click Me!", command=lambda: button_function(nome_usuario))
 button.pack()
+
+#check button
+check = ttk.Checkbutton(window, text="Check me!", variable=check_var)
+check.pack()
+
+#radio button
+radio_var1 = ttk.Radiobutton(window, text="radio button 1", value=1)
+radio_var1.pack()
+radio_var2 = ttk.Radiobutton(window, text="radio button 2", value=1, variable=check_radio_var)
+radio_var2.pack()
+radio_var3 = ttk.Radiobutton(window, text="radio button 3", value=3)
+radio_var3.pack()
+radio_var4 = ttk.Radiobutton(window, text="radio button 4", value=2, variable=check_radio_var)
+radio_var4.pack()
+radio_var5 = ttk.Radiobutton(window, text="radio button 5", value=5)
+radio_var5.pack()
 
 window.mainloop()
