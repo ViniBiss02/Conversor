39718794031e07072d3f550e7c01b9b0abeeb5b0
ViniBiss02 <viniciusbiss02@gmail.com>
2025-05-11 00:23:17 -0300

    aprendendo sobre o uso do command função com parameter.

--- learn_tkinter.py
+++ learn_tkinter.py
@@ -1,38 +1,26 @@
 import tkinter as tk
 from tkinter import ttk
 
-nome_usuario = "vinicius"
+def button_function(entry_string):
+    print("Button clicked!")
+    print(entry_string.get())
+
+def outer_func(parameter):
+    def inner_func():
+        print("Inner function called...")
+        print(parameter.get())
+    return inner_func
 
 window = tk.Tk()
 window.title("Learn Tkinter")
-window.geometry("600x400")
 
-check_var = tk.BooleanVar()
-check_radio_var = tk.BooleanVar()
+#widgets
+entry_string = tk.StringVar(value = 'Test')
+entry = ttk.Entry(window, textvariable=entry_string)
+entry.pack()
 
-def button_function(nome):
-    if check_var.get() and check_radio_var.get() == True:
-        print(f"hello {nome} - check is checked")
-    else:
-        print(f"hello {nome} - check is not checked")
-
-button = ttk.Button(master = window, text="Click Me!", command=lambda: button_function(nome_usuario))
+button = ttk.Button(window, text="Click Me", command=outer_func(entry_string))
 button.pack()
 
-#check button
-check = ttk.Checkbutton(window, text="Check me!", variable=check_var)
-check.pack()
-
-#radio button
-radio_var1 = ttk.Radiobutton(window, text="radio button 1", value=1)
-radio_var1.pack()
-radio_var2 = ttk.Radiobutton(window, text="radio button 2", value=1, variable=check_radio_var)
-radio_var2.pack()
-radio_var3 = ttk.Radiobutton(window, text="radio button 3", value=3)
-radio_var3.pack()
-radio_var4 = ttk.Radiobutton(window, text="radio button 4", value=2, variable=check_radio_var)
-radio_var4.pack()
-radio_var5 = ttk.Radiobutton(window, text="radio button 5", value=5)
-radio_var5.pack()
-
+window.geometry("400x300")
 window.mainloop()
